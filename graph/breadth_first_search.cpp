f794fba57cc078edf20afd92dfa33261412e7f32
Lenar Sharipov <lsharipov@gmail.com>
2019-07-11 21:15:19 +0200

    Added breadth first search algorithm

---
+++ graph/breadth_first_search.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+#include <string>
+#include <queue>
+
+#include "array_utility.h" // print_array
+
+using namespace std;
+
+
+
+class Node
+{
+public:
+    string name;
+    vector<Node *> children;
+
+    Node(string str)
+    {
+        name = str;
+    }
+
+    vector<string> breadthFirstSearch(vector<string> *array)
+    {
+        queue<Node *> line;
+
+        line.push(this);
+
+        while(!line.empty()) {
+            auto item = line.front();
+            line.pop();
+
+            array->push_back(item->name);
+
+            for (auto child : item->children) {
+                line.push(child);
+            }
+        }
+
+        return *array;
+    }
+
+    Node *addChild(string name)
+    {
+        Node *child = new Node(name);
+        children.push_back(child);
+        return this;
+    }
+};
+
+int main()
+{
+    Node root("a");
+    root.addChild("b")
+        ->addChild("c")
+        ->addChild("d");
+
+    root.children[0]->addChild("e");
+
+    vector<string> array;
+
+    root.breadthFirstSearch(&array);
+
+    print_array(array);
+
+    return 0;
+}
