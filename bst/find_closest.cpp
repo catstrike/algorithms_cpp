cae7ebdf74cb9f521eabad6afb190a906ba09b9a
lsharipov <lsharipov@megazebra.com>
2019-07-15 18:17:55 +0200

    Added solution for 'find closest value in bst'

---
+++ bst/find_closest.cpp
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+class BST {
+  public:
+    int value;
+    BST* left;
+    BST* right;
+
+    BST(int val);
+    BST& insert(int val);
+};
+
+int traverse(BST* node, int target, int best, int bestDistance)
+{
+	if (node == nullptr) {
+		return best;
+	}
+	
+	auto value = node->value;
+	
+	if (value == target) {
+		return value;
+	}
+	
+	auto distance = abs(value - target);
+	
+	if (distance < bestDistance) {
+		best = value;
+		bestDistance = distance;
+	}
+	
+	return (target > value)
+		? traverse(node->right, target, best, bestDistance)
+		: traverse(node->left, target, best, bestDistance);
+}
+
+
+int findClosestValueInBst(BST* tree, int target) {
+	auto value = tree->value;
+	auto distance = abs(value - target);
+	
+  return traverse(tree, target, value, distance);
+}
