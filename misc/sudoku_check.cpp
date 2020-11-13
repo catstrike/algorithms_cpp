82b2e81303b31ac3fac865e3a2e246b6daf04c72
Lenar Sharipov <lsharipov@gmail.com>
2020-11-14 00:42:30 +0100

    Added a couple of test cases for Sudocku Checker

--- misc/sudoku_check.cpp
+++ misc/sudoku_check.cpp
@@ -81,8 +81,38 @@ int main()
         {9, 7, 6,  3, 4, 1,  8, 2, 5}
     };
 
+    vector<vector<int>> board2 {
+        {2, 4, 5,  1, 3, 6,  7, 9, 8},
+        {1, 8, 7,  4, 9, 2,  3, 5, 6},
+        {3, 6, 9,  8, 5, 7,  2, 4, 1},
+
+        {1, 7, 4,  9, 6, 2,  3, 5, 8},
+        {5, 9, 8,  7, 3, 4,  2, 1, 6},
+        {2, 3, 6,  5, 8, 1,  7, 4, 9},
+        
+        {6, 1, 3,  2, 4, 9,  5, 8, 7},
+        {2, 7, 4,  5, 1, 8,  6, 9, 3},
+        {5, 8, 9,  3, 6, 7,  1, 2, 4}
+    };
+
+    vector<vector<int>> board3 {
+        {7, 4, 2,  6, 1, 3,  9, 5, 8},
+        {1, 8, 5,  4, 9, 2,  3, 6, 7},
+        {9, 6, 3,  8, 5, 7,  4, 2, 1},
+
+        {4, 7, 1,  9, 6, 5,  8, 3, 2},
+        {3, 9, 8,  7, 2, 4,  5, 1, 6},
+        {2, 5, 6,  3, 8, 1,  7, 4, 9},
+
+        {6, 3, 7,  2, 4, 9,  1, 8, 5},
+        {8, 1, 4,  5, 7, 6,  2, 9, 3},
+        {5, 2, 9,  1, 3, 8,  6, 7, 4}
+    };
+
     cout<<"The board 0 is "<<(sudokuCheck(board0) ? "valid" : "invalid")<<endl;
     cout<<"The board 1 is "<<(sudokuCheck(board1) ? "valid" : "invalid")<<endl;
+    cout<<"The board 2 is "<<(sudokuCheck(board2) ? "valid" : "invalid")<<endl;
+    cout<<"The board 3 is "<<(sudokuCheck(board3) ? "valid" : "invalid")<<endl;
 
     return 0;
 }
